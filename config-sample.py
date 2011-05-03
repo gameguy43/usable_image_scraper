4896ce6a4590ebc2895b7bee5af0a6c0bcde845c
Parker Phinney <parker.phinney@gmail.com>
2011-05-03 16:14:29 -0400

    updated config

--- config-sample.py
+++ config-sample.py
@@ -1,18 +1,36 @@
-DATA_ROOT = '/home/rod/data/cdc-phil-data/'
-THUMB_IMG_DIR = DATA_ROOT + 'thumbs'
-LORES_IMG_DIR = DATA_ROOT + 'lores'
-HIRES_IMG_DIR = DATA_ROOT + 'hires'
-RAW_HTML_DIR = DATA_ROOT + 'raw-html'
+image_databases = {
+    "cdc_phil": {
+        'python_lib': 'cdc_phil_lib',
+        'data_subdir': 'cdc_phil/', #trailing slash
+        'data_table_prefix': 'cdc_phil_',
+        'long_name' : "The Center for Disease Control's Public Health Image Library",
+        'homepage' : "http://phil.cdc.gov/",
+        'code_url' : "https://github.com/gameguy43/cdc_phil_lib",
+        },
+    "fema": {
+        'python_lib': 'fema_lib',
+        'data_subdir': 'fema/', #trailing slash
+        'data_table_prefix': 'fema_',
+        'long_name' : "The Fema Photo library",
+        'homepage' : "http://www.fema.gov/photolibrary/",
+        'code_url' : "https://github.com/gameguy43/fema_lib",
+        },
+    }
 
-# number of downloaders to run
-MAX_DAEMONS = 50
+## Set these as needed locally
+data_root_dir = '/home/pyrak/workspace/usable_images/usable_image_scraper/data/'
+thumb_subdir = 'thumbs/'
+lores_subdir = 'lores/'
+hires_subdir = 'hires/'
+html_subdir = 'html/'
+max_daemons = 50
+#max_daemons = 1
+import sites
+img_libraries_metalib = sites
 
-data_mysql_db_host = 'localhost'
-data_mysql_db_db = 'cdc_phil_data'
-data_mysql_db_user = 'phil'
-data_mysql_db_pass = 'toast'
+data_db_host = ''
+data_db_db = data_root_dir + 'metadata.sqlite'
+data_db_user = ''
+data_db_pass = ''
 
-data_sqlite_db_host = ''
-data_sqlite_db_db = DATA_ROOT + 'metadata.sqlite'
-data_sqlite_db_user = ''
-data_sqlite_db_pass = ''
+db_url = 'sqlite:///' + data_db_db
