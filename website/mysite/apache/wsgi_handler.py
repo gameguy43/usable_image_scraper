5fc975c4962eb4732f86bc79decf769dd8c93be2
Parker Phinney <parker.phinney@gmail.com>
2011-05-31 18:28:34 -0400

    removed website from repo. on second thought, they should stay in separate repos for now

--- website/mysite/apache/wsgi_handler.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-#sys.path.append('/usr/local/django')
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
